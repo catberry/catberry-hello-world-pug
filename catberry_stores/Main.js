3d18c93e7ae71dc46a30c34f3f67403523052398
Denis Rechkunov <denis.rechkunov@gmail.com>
2017-03-19 17:26:54 +0100

    Initial commit.

---
+++ catberry_stores/Main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class Main {
+
+	/**
+	 * Loads data from somewhere.
+	 * @returns {Object} Data object.
+	 */
+	load() {
+		return {
+			who: 'World'
+		};
+	}
+}
+
+module.exports = Main;
